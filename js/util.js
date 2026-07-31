9d8f01437e9f588f6ddd8185224fa2ec79ea0bee
makskwiat1512-bit <makskwiat1512@gmail.com>
2026-07-31 10:39:31 +0100

    final edit before merging?

--- js/util.js
+++ js/util.js
@@ -2,9 +2,9 @@
 export function getYoutubeIdFromUrl(url) {
     if (!url) return '';
     
-    // If it's a Google Drive link, return a special tag instead of breaking
-    if (url.includes('://google.com')) {
-        return 'googledrive';
+    // If it's a Google Drive link, return a special hack to escape the YouTube domain template
+    if (url.includes('drive.google.com')) {
+        return `../../../../${url.replace('https://', '').replace('/view', '/preview')}`;
     }
 
     return url.match(
@@ -16,11 +16,11 @@ export function embed(video) {
     if (!video) return '';
 
     // If it's a Google Drive link, bypass YouTube logic entirely >w<
-    if (video.includes('://google.com')) {
+    if (video.includes('drive.google.com')) {
         return video.replace('/view', '/preview');
     }
 
-    // Default template logic for YouTube videos
+    // Default template logic for YouTube videos (Added the missing $)
     return `https://youtube.com{getYoutubeIdFromUrl(video)}`;
 }
 
@@ -29,8 +29,10 @@ export function localize(num) {
 }
 
 export function getThumbnailFromId(id) {
-    // If there is no YouTube ID (like when using Google Drive), return a blank transparent image layout placeholder
-    if (!id) return 'data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7';
+    // If there is no YouTube ID or it's our Google Drive hack string, return a blank transparent layout placeholder
+    if (!id || id.includes('drive.google.com')) {
+        return 'data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7';
+    }
     
     return `https://img.youtube.com/vi/${id}/mqdefault.jpg`;
 }
